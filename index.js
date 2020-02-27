f6935ea68c0e9436947de13498b3e8c7f3f90c67
Demons Light <jetaimefrc@gmail.com>
2020-02-27 22:21:12 +0700

    feat: upgrade to download image

--- index.js
+++ index.js
@@ -1,31 +1,20 @@
-const fs = require('mz/fs')
+const fs = require('fs')
 const cheerio = require('cheerio')
 const imgDownload = require('image-downloader')
 
-/**
- * Get all file path in local
- * @param {String} folderName
- */
-async function getFilePath(folderName, result = []) {
-	await fs.readdirSync(folderName).forEach((filePath, i) => {
-		result[i] = filePath
-	})
-	return result
-}
-
-// const orginFile = ['./originFile/1.xml', './originFile/2.xml', './originFile/3.xml']
+const resultFolder = __dirname + '/result'
+const fileName = __dirname + '/temp.txt'
 
 /**
  * To get all urls of image in a file
  * @param {String} filePath
  */
-async function getUrlImagesFromFile(filePath, listUrl = []) {
-	const temp = await fs.readFileSync(filePath, { encoding: 'utf8' }, (err, data) => {
-		return data
-	})
-	const $ = cheerio.load(temp)
-	$('img').each((i, e) => {
-		listUrl[i] = e.attribs.src.toString()
+const getUrlImagesFromFile = filePath => {
+	let listUrl = []
+	const data = fs.readFileSync(filePath, { encoding: 'utf8' })
+	const $ = cheerio.load(data)('img')
+	$.each((i, e) => {
+		listUrl[i] = e.attribs.src
 	})
 	return listUrl
 }
@@ -34,29 +23,22 @@ async function getUrlImagesFromFile(filePath, listUrl = []) {
  * Download to a directory and save with the original filename
  * @param {Array} listUrl
  */
-async function downloadImages(listUrl, resulFolder) {
-	listUrl.forEach(img => {
+const downloadImages = async (listUrl, resulFolder) => {
+	for (const imageUrl of listUrl) {
 		let options = {
-			url: img,
+			url: imageUrl,
 			dest: resulFolder,
 		}
-		imgDownload(options)
-	})
+		await imgDownload(options)
+	}
 }
 
-async function main() {
-	const folderName = './originFile'
-	const orginFile = await getFilePath(folderName)
-	orginFile.forEach(async (filePath, i) => {
-		const resultFolder = `./result_${i}`
-		if (!fs.existsSync(resultFolder)) {
-			fs.mkdirSync(resultFolder)
-		}
-		const fileName = folderName + '/' + filePath
-		const listUrl = await getUrlImagesFromFile(fileName)
-		console.log(listUrl)
-		await downloadImages(listUrl, resultFolder).catch(err => console.log(err))
-	})
+const main = () => {
+	if (!fs.existsSync(resultFolder)) {
+		fs.mkdirSync(resultFolder)
+	}
+	const listUrl = getUrlImagesFromFile(fileName)
+	downloadImages(listUrl, resultFolder).catch(err => console.log(err))
 }
 
 main()
